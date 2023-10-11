7f51d35a1732451465745ab79bc1932d1e436585
Vikramvicky00 <59464502+Vikramvicky00@users.noreply.github.com>
2023-10-11 17:56:46 +0530

    Basic C# DataType Program

--- Assignments/BasicDataTypes.cs
+++ Assignments/BasicDataTypes.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharpBasics
+namespace Assignments
 {
     internal class BasicDataType
     {
